1ab01a4103e2a0b0027c81c06bf2d2d59a7ab7ca
Julie Kim <juliekimjiehyun@gmail.com>
2023-07-08 04:44:28 -0700

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
-app.arguments((req, res) => {
-    console.log(req.headers);
+app.use(express, statis(__dirname + '/public'));
+
+app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
